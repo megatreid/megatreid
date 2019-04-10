c4a92abfce732d397f8b94ecb990ac08d5773663
Sergey <33266120+wolfs2003@users.noreply.github.com>
2019-04-10 11:32:21 +0300

    Update report_contr_abonent.php

--- reports/report_contr_abonent.php
+++ reports/report_contr_abonent.php
@@ -22,7 +22,7 @@ $sheet->setCellValue('A2','Отчетный год:');
 $sheet->setCellValue('A3','Отчетный период:');
 $sheet->setCellValue('A4','Кол-во месяцев:');
 $sheet->setCellValue('A5','Способ оплаты:');
-$sheet->setCellValue('B5',$methodpaymentedit[$method_payment+1]);
+$sheet->setCellValue('B5',$methodpaymentedit[$method_payment]);
 $sheet->setCellValue('B2',$year);
 $sheet->setCellValue('B3',($month_start_name." - ".$month_end_name));
 $sheet->setCellValue('B4',($month_period));
